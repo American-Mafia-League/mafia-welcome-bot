d79f2e0528a3f9b6eed88961ceefeb1d118c0f8a
sumtopmus <sumtopmus@users.noreply.github.com>
2024-07-20 16:08:41 -0400

    Added more info displayed upon /info command

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -15,8 +15,12 @@ def create_handlers() -> list:
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Basic admin info command."""
     log('info')
-    log(f'chat_id: {update.effective_chat.id}', level=logging.INFO)
-    log(f'user_id: {update.effective_user.id}', level=logging.INFO)
+    log(f'chat id: {update.effective_chat.id}', level=logging.INFO)
+    log(f'chat title: {update.effective_chat.title}', level=logging.INFO)
+    log(f'chat username: {update.effective_chat.username}', level=logging.INFO)
+    log(f'user id: {update.effective_user.id}', level=logging.INFO)
+    log(f'user name: {update.effective_user.username}', level=logging.INFO)
+    log(f'user full name: {update.effective_user.full_name}', level=logging.INFO)  
     try:
         await update.message.delete()
     except BadRequest as e:
